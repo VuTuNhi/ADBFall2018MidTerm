0d14a25d119a7f4b2cb773b069bacf8404bd4b0a
nhiem098 <38127774+nhiem098@users.noreply.github.com>
2018-11-07 08:56:10 +0700

    Update K4201104238.sql

--- K4201104238.sql
+++ K4201104238.sql
@@ -3,7 +3,9 @@ SELECT Ma, YNghia, ViTri.STArea() as DienTich,
 	ViTri.STArea() * LuongNuoc as LuongNuocTuoiTieu
 FROM dbo.qldatdai;
 
-
+cau b
+SELECT Max(DienTich) FROM (SELECT Ma, YNghia, ViTri.STArea() as DienTich
+FROM dbo.qldatdai) ;
 
 cau c
 DECLARE @kenh geometry = geometry::STGeomFromText('POLYGON ((1 3,4 -2,  0 0,1 3))', 0);
